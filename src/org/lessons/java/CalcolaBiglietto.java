636bff532d4563593fc992461970b0648630a99a
antonionovelli92 <anto.novelli@hotmail.it>
2023-05-04 15:30:07 +0200

    add 2' ex 'CibiPreferiti'

--- src/org/lessons/java/CalcolaBiglietto.java
+++ src/org/lessons/java/CalcolaBiglietto.java
@@ -39,3 +39,4 @@ public class CalcolaBiglietto {
         scanner.close();
     }
 }
+
---
+++ src/org/lessons/java/CibiPreferiti.java
@@ -0,0 +1,23 @@
+package org.lessons.java;
+
+public class CibiPreferiti {
+	  public static void main(String[] args) {
+	        // list
+	        String[] classificaCibi = {"Pizza", "Panino", "Sushi", "Carbonara", "Gelato", "Antonio", "Cavolfiore"};
+
+	        // Class
+	        System.out.println("La classifica è: " + classificaCibi.length);
+
+	        // top
+	        System.out.println("Il mio cibo top è: " + classificaCibi[0]);
+
+	        // last
+	        System.out.println("Il mio cibo preferito ma non troppo è: " + classificaCibi[classificaCibi.length - 1]);
+
+	        // middle (check if 'odd')
+	        if (classificaCibi.length % 2 != 0) {
+	            int middle = classificaCibi.length / 2;
+	            System.out.println("Il cibo di mezza classifica è: " + classificaCibi[middle]);
+	        }
+	    }
+}
