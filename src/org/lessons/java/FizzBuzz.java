f5c909730e1fd1fbf4aa2cf39cfafe0028056907
antonionovelli92 <anto.novelli@hotmail.it>
2023-05-04 17:16:04 +0200

    fix 3' ex 'FizzBuzz'

--- src/org/lessons/java/FizzBuzz.java
+++ src/org/lessons/java/FizzBuzz.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 
 public class FizzBuzz {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
 
         System.out.print("Inserisci un numero casuale: ");
-        int numbUser = sc.nextInt();
+        int numbUser = scan.nextInt();
 
         for (int i = 1; i <= numbUser; i++) {
             if (i % 3 == 0 && i % 5 == 0) {
@@ -20,5 +20,6 @@ public class FizzBuzz {
                 System.out.println(i);
             }
         }
+        scan.close();
     }
 }
