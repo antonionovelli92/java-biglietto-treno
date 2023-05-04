e857dc524137c82a1948cf1da16ef92d9328f5fd
antonionovelli92 <anto.novelli@hotmail.it>
2023-05-04 17:35:51 +0200

    add 4' ex 'CheckGuest'

---
+++ src/org/lessons/java/CheckGuest.java
@@ -0,0 +1,45 @@
+package org.lessons.java;
+
+import java.util.Scanner;
+
+public class CheckGuest {
+    public static void main(String[] args) {
+        String[] invitati = {
+        		"Dua Lipa", 
+        		"Paris Hilton", 
+        		"Manuel Agnelli", 
+        		"J-Ax", 
+        		"Francesco Totti",
+                "Ilary Blasi", 
+                "Bebe Vio", 
+                "Luis", 
+                "Pardis Zarei", 
+                "Martina Maccherone", 
+                "Rachel Zeilic",
+                "Antonio Novelli"
+                };
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Inserisci il tuo nome: ");
+        String nome = scanner.nextLine();
+
+//        Non so ancora se l'utente è invitato alla festa
+        boolean presente = false;
+//        faccio partire il tutto da 0 (perchè è un arrey di nomi)
+        int i = 0;
+//        Verifico se il nome è presente
+        while (!presente && i < invitati.length) {
+            if (invitati[i].equalsIgnoreCase(nome)) {
+                presente = true;
+            }
+            i++;
+        }
+
+        if (presente) {
+            System.out.println("Benvenuto alla festa!");
+        } else {
+            System.out.println("Mi dispiace, non sei sulla lista degli invitati.");
+        }
+        scanner.close();
+    }
+}
